8517b54f9bee19ec0b710c7b945917ac54d6f327
Lautaro Bachmann <laurigbach@gmail.com>
2022-10-03 10:09:59 -0300

    Pequeños cambios en el nombre de un macro y en el formato del pong - Cambiar nombre del macro para que se entienda mejor lo que hace
    - Añadir tab al 'pong' para que sea mas facil de diferenciar del 'ping'

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,56 +2,56 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define exit_error(error) if(error == 0){printf("Error in pingpong\n");return 0;}
+#define exit_if_zero(error) if(error == 0){printf("Error in pingpong\n");return 0;}
 
 int
 main(int argc, char *argv[])
 {
   if(argc != 2){
-    exit_error(0);
+    exit_if_zero(0);
 
   }
 
   int error = 1;
 
   error = sem_open(0,1); // Ping
-  exit_error(error);
+  exit_if_zero(error);
   error = sem_open(1,0); // Pong
-  exit_error(error);
+  exit_if_zero(error);
 
   int N = atoi(argv[1]);
   
   int pid = fork();
   if(pid < 0){
-    exit_error(0);
+    exit_if_zero(0);
 
   }else if(pid == 0){
     for(unsigned int i = 0; i < N; i++){
       error = sem_down(0);
-      exit_error(error);
+      exit_if_zero(error);
       
       printf("ping\n");
 
       error = sem_up(1);
-      exit_error(error);
+      exit_if_zero(error);
     }
 
   }else{
     for(unsigned int i = 0; i < N; i++){
       error = sem_down(1);
-      exit_error(error);
+      exit_if_zero(error);
 
-      printf("pong\n");
+      printf("\tpong\n");
 
       error = sem_up(0);
-      exit_error(error);
+      exit_if_zero(error);
     }
 
     error = sem_close(0);
-    exit_error(error);
+    exit_if_zero(error);
 
     error = sem_close(1);
-    exit_error(error);
+    exit_if_zero(error);
   }
 
   return 1;
